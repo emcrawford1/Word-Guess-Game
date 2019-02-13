48e62c13e96275ccbff6998896b86a9f6b4cd36b
mrCrawford <noreply>
2019-02-12 23:22:08 -0600

    Game is complete.  There might be a bug or two but it works for the most part.  Might also want to refactor at some point especially since this was my first time using objects for a large project.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,17 +17,32 @@ var game = {
         'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
 
 
+    //Jet move properties - these properties are specific to making the jet move.
+
+    yCoord: 40, //Top 
+    xCoord: 25, //Left
+    degShift: 0,
+    jet: document.getElementById('freeFloat'),
+    degString: "",
+    xString: "",
+    yString: "",
+
+
+    //Change background properties - these properties change the background when the jet crashes.
+    changeBackGround: 0,
+    backDropSwitch: document.getElementById('backDrop'),
 
 
 
-    //Data validation function that will check the user's input to ensure it is a valid char (a-z).  If 
+
+    //The mainFunction() will check the user's input to ensure it is a valid char (a-z).  If 
     //not it prompt an alert that notify the user to press the appropriate key.  This function also acts
     //to call other functions and acts as the jumping off point for the entire program as it is called by
     //eventlistener outside of the object and it calls other functions in the program.
 
     mainFunction: function (event) {
 
-       game.jetMaster();
+        if(game.remainingGuesses > 0){
         game.gameInstructions();
 
         var charTest = event.key.toUpperCase();
@@ -46,6 +61,7 @@ var game = {
 
             game.userLetterGuess = charTest;
 
+            //Runs the userGuessAssign function and checks the return value.  If false it runs the compare KeyToWord function.
             if (!game.userGuessAssign()) {
 
                 if (!game.compareKeyToWord()) {
@@ -56,20 +72,23 @@ var game = {
 
                     if (game.remainingGuesses < 1) {
                         game.loseInstructions();
-                        game.resetGame();
+                        game.jetMaster(false);
+                        
                     }
                 }
 
                 else {
                     if (game.correctTracker === game.guessWord.length) {
                         game.winInstructions();
-                        game.resetGame();
+                        game.jetMaster(true);
+                       
                     }
                 }
 
             }
 
         }
+    }
 
     },
 
@@ -165,7 +184,7 @@ var game = {
     },
 
 
-    //Delete the old word and create a new word.  All nodeswith <span class="guessChar"> will be selected, 
+    //Delete the old word and create a new word.  All nodes with <span class="guessChar"> will be selected, 
     //deleted, then repopulated with a new word.  The second block of this function deletes the user's 
     //previous guesses.
 
@@ -192,10 +211,24 @@ var game = {
     resetGame: function () {
         this.replaceNodes();
         this.lettersGuessed = [];
-        this.remainingGuesses = 5;
         this.correctTracker = 0;
         var guessRemain = document.getElementById('guessesRemaining');
+        game.remainingGuesses = 5;
         guessRemain.textContent = "Guesses Remaining: " + game.remainingGuesses;
+        game.degString = "0";
+        game.jet.style.transform = "rotate(" + game.degString + "deg)";
+        game.jet.transform = "rotate(0deg)";
+        game.jet.style.top = "40px";
+        game.jet.style.left = "25px";
+        game.backDropSwitch.style.backgroundColor = "rgba(16, 189, 233, 0.986)";
+        game.changeBackGround = 0;
+        game.yCoord = 40;  
+        game.xCoord = 25;
+        game.degShift = 0;
+        game.degString = "";
+        game.xString = "";
+        game.yString = "";
+        game.remainingGuesses = 5;
     },
 
 
@@ -232,44 +265,29 @@ var game = {
     },
 
 
-    //Jet move properties - these properties are specific to making the jet move
 
-    yCoord: 40, //Top 
-    xCoord: 25, //Left
-    degShift: 0,
-    jet: document.getElementById('freeFloat'),
-    degString: "",
-    xString: "",
-    yString: "",
-
-
+    //Master jet function.  This function simply calls the appropriate jet function depending on
+    //if the user won the game.
 
     jetMaster: function (win) {
 
-
-
-        game.moveJetLost();
-
         if (win) {
-
-
-
-
+            game.moveJetWin();
         }
 
         if (!win) {
-
+            game.moveJetLost();
         }
 
-
-
-
     },
-    
-   
-    //If the player wins the jet flies off into the sunset (blue sky)
 
-    moveJetWin: function() {
+
+    //If the player wins the jet flies off into the sunset (i.e. blue noon sky).  This function
+    //iterates the jet's coordinates (the jet is positioned absolutel on the page by a specified  
+    //amount and uses the setTimeOut to recursivelycall the function and slow down the calls.  
+    //This way you can see the jet move across the screen.
+
+    moveJetWin: function () {
 
         xString = game.xCoord.toString();
 
@@ -284,12 +302,18 @@ var game = {
             timeOut = setTimeout(game.moveJetWin, 5);
         }
 
+        else{
+            game.resetGame();
+        }
+
     },
 
 
-    //If the player loses crash the jet into the ground
+    //If the player loses crash the jet into the ground.  Like the moveJetWin() function above,
+    //this function uses a recursive call from the setTimeOut function to slow the loop so that 
+    //the user can "see" the jet fly across the screen.
 
-    moveJetLost: function() {
+    moveJetLost: function () {
 
         game.yString = game.yCoord.toString();
         game.xString = game.xCoord.toString();
@@ -302,28 +326,73 @@ var game = {
         game.jet.style.top = game.yString;
         game.jet.style.transform = "rotate(" + game.degString + "deg)";
 
-        if(game.xCoord < 750){
-        game.xCoord = game.xCoord + 5;
+        if (game.xCoord < 750) {
+            game.xCoord = game.xCoord + 5;
         }
-        if(game.xCoord > 700){
-           
-            if(game.yCoord < 300 && game.degShift < 90){
+
+        //This is the point in the function where the jet needs to make a 90 degree turn
+        //and crash into the ground.  This portion of the function runs after the jet has
+        //passed the 700 x-coordinate point.
+
+        if (game.xCoord >= 700) {
+
+            //This portion of the function orients the jet so that it will begin moving 
+            //downward and crash into the ground.  It also shift the jets orientation so
+            //that it is now facing downward (degShift)
+
+            if (game.yCoord < 300 && game.degShift < 90) {
                 game.yCoord = game.yCoord + 5;
                 game.xCoord = game.xCoord + 5;
                 game.degShift = game.degShift + 5;
-                console.log(game.degShift);
             }
-            else{
+            else {
                 game.yCoord += 5;
-    
+
             }
         }
 
+        //Recursive call to function with setTimeOut
         if (game.yCoord < 700) {
 
             timeOut = setTimeout(game.moveJetLost, 5);
         }
 
+        //Runs after all recursive calls have been made
+        else {
+            //Calls the backGroundChange function which will change the background when the 
+            //jet crashes.
+            game.backGroundChange();
+        }
+
+    },
+
+   
+    
+    //If the user loses, this function causes the background to flicker black and red to simulate
+    //the jet crashing.  As with the other animation functions this function uses a recursive call
+    //with the setTimeOut function to allow the user to see the flicker.
+
+    backGroundChange: function(){
+
+        if(game.changeBackGround % 2 === 0){
+        game.backDropSwitch.style.backgroundColor = "red";
+    }
+
+        else{
+            game.backDropSwitch.style.backgroundColor = "black";
+        }
+        
+        game.changeBackGround++;
+
+        if(game.changeBackGround < 50){
+            var timeOut = setTimeout(game.backGroundChange, 30);
+        }
+        
+        else{
+            game.resetGame();
+        }
+
+     
     }
 
 
